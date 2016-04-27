433ebcb4f650c828286d390b584781e45c5f23fe
Stasyyan <stasya88888@yandex.ru>
2016-04-27 20:02:06 +0300

    Create module2.2.5.c

---
+++ module2.2.5.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main( void )
+{
+  float num1, num2, sum, mul, sub, cut;
+  printf("Input numbers: ");
+  scanf(%f %f, &num1, &num2);
+  printf("Sum: %f, mul: %f, sub: %f, cut: %f", num1 + num2, num1 * num2, num1 - num2, num1 / num2);
+  return(0);
+}

1fb4c66d6c9fc15685acc7844d38b3fe2f0327ff
Stasyyan <stasya88888@yandex.ru>
2016-04-27 20:03:34 +0300

    Create module2.2.7.c

---
+++ module2.2.7.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main( void )
+{
+  int k, i, a = 256, b = 2455, d = 55552, c = 141;
+  printf("Input the length of sequence: ");
+  scanf("%i", &k);
+  for (i = 0; i < k; i++)
+    printf("%i ", (a*b + с)%d);
+  return(0);  
+}

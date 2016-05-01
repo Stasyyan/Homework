fe175f0148249b42702195e70c6d5cf262057255
Stasyyan <stasya88888@yandex.ru>
2016-05-01 17:52:44 +0300

    Create 2.5.5.6.c

---
+++ module2.5/2.5.5.6.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <assert.h>
+#include  "Task2_5_5_1.c"
+int main() {
+	int n,k,j,c, l=0;
+	float D,d;
+	int **matr;
+	printf("Введите размерность квадратной матрицы: ");
+	scanf("%d", &n);
+	matr=(int**)malloc(sizeof(int*)*n);
+	for(k=0;k<n;k++) 
+	{
+	matr[k]=(int*)malloc(sizeof(int)*n);
+		for(j=0;j<n;j++) 
+		{
+			printf("Элемент [%d][%d]= ",k+1,j+1);
+			scanf("%d",&matr[k][j]);
+		}
+	}
+	D=Det(matr,n);
+	assert(D!=0);
+	int svob[n];  
+	printf("Введите столбец свободных коэффициентов:\n");
+	for(k=0;k<n;k++)
+	{
+		printf("Элемент [%d] = ",k+1);
+		scanf ("%d",&svob[k]);
+	}
+	printf("Корни системы уравнений:\n");
+	for(k=0;k<n;k++)
+	{
+		for(j=0;j<n;j++)
+		{
+			c=matr[j][k];
+			matr[j][k] = svob[j];
+			svob[j]=c;
+		}
+		d=Det(matr,n);
+		l++;
+		printf("x%d=%f\n",k+1, d/D);
+		for(j=0;j<n;j++)
+		{
+			c=svob[j];
+			svob[j] = matr[j][k];
+			matr[j][k]=c;
+		}
+	}
+	printf("Количество вызовов программы  для определителя: %d\n",l);
+	return 0;
+}

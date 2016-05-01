5553b957cb2b42b5cb3bc7fa512b4aed3f022fef
Stasyyan <stasya88888@yandex.ru>
2016-05-01 14:51:15 +0300

    Update 2.4.5.c

--- module2.4/2.4.5.c
+++ module2.4/2.4.5.c
@@ -10,8 +10,8 @@ j=a+n;
 r=d+s;
 l=a*n-d*s;
 k=a*s*+d*n;
-R1=(l*j-r*k)/(j*j+r*r);
-R2=(r*l+j*k)/(j*j+r*r);
+R1=(l*j-r*k)\(j*j+r*r);
+R2=(r*l+j*k)\(j*j+r*r);
 printf("Cопротивление %.2d + (%.2d)*i \n",R1,R2 );
 return 0;
 }

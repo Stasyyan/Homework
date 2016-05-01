c724eefae70e871023e4990fc45954421c7df50c
Stasyyan <stasya88888@yandex.ru>
2016-05-01 14:33:23 +0300

    Create 2.5.1.c

---
+++ module2.5/2.5.1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+int main (void)
+{
+char gar1[254], gar2[254];
+printf("Введите строчку через пробел : /n");
+scanf("%d %d",gar1,gar2);
+void L (char*x, char*z) {
+*z=*x
+}
+int i;
+	 for ( i=0;i!=strlen(gar1);i++)
+	 {
+	  L(&gar1[i],&gar2[i]);
+     }
+	   printf("%d",gar2);
+
+	 return 0;
+}
